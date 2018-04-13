3188603c1c2c45da42248535b458484d9a2ef597
gitrafee <31915731+gitrafee@users.noreply.github.com>
2018-04-13 03:51:37 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
-      System.out.println("Qick Change");
-      System.out.println("Anil bidari");
+      System.out.println("Testing Docker Auto build");
    }
 }
